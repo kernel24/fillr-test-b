b04e0d264d2d23e8fe8c1db16fedb59d17554afc
JK Jung <kernel24@gmail.com>
2017-11-26 05:12:01 +0900

    Added launcher for chrome without security

--- karma.conf.js
+++ karma.conf.js
@@ -65,8 +65,14 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
-
+    browsers: ['Chrome_without_security'],
+    
+    customLaunchers: {
+      Chrome_without_security: {
+        base: 'Chrome',
+        flags: ['--disable-web-security', '--allow-running-insecure-content']
+      }
+    },
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
